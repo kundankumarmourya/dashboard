766c3eadec493fa2f2daf0639a23166604a6ff5b
Kundan Kumar Mourya <32189172+kundankumarmourya@users.noreply.github.com>
2019-09-10 20:12:56 +0530

    google material import all module in app-module

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # App Dashboard
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.3.2.
 
 ## Development server
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+// Angular Components start
+import {
+  MatAutocompleteModule, MatBadgeModule,  MatBottomSheetModule,  MatButtonModule,  MatButtonToggleModule,  MatCardModule,  MatCheckboxModule, MatChipsModule,  MatDatepickerModule, MatDialogModule,  MatDividerModule,  MatExpansionModule,
+  MatGridListModule,  MatIconModule,  MatInputModule,  MatListModule,  MatMenuModule,  MatNativeDateModule, MatPaginatorModule,  MatProgressBarModule,  MatProgressSpinnerModule,  MatRadioModule,  MatRippleModule, MatSelectModule,  MatSidenavModule,  MatSliderModule,  MatSlideToggleModule,  MatSnackBarModule,  MatSortModule,
+  MatStepperModule,  MatTableModule,  MatTabsModule,  MatToolbarModule,  MatTooltipModule,  MatTreeModule,
+} from '@angular/material';
+// Angular Components end
+// core file inport up
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { MainPageComponent } from './main-page/main-page.component';
+
 
 @NgModule({
   declarations: [
@@ -16,7 +28,14 @@ import { MainPageComponent } from './main-page/main-page.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    // Angular Components start
+    MatAutocompleteModule, MatBadgeModule, MatBottomSheetModule, MatButtonModule, MatButtonToggleModule,
+    MatCardModule, MatCheckboxModule,    MatChipsModule, MatStepperModule, MatDatepickerModule, MatDialogModule,    MatDividerModule, MatExpansionModule, MatGridListModule, MatIconModule,MatInputModule, MatListModule, MatMenuModule,
+    MatNativeDateModule, MatPaginatorModule, MatProgressBarModule,MatProgressSpinnerModule, MatRadioModule,  MatRippleModule,  MatSelectModule,  MatSidenavModule, MatSliderModule,  MatSlideToggleModule,  MatSnackBarModule,
+    MatSortModule, MatTableModule, MatTabsModule, MatToolbarModule, MatTooltipModule,MatTreeModule,
+    // Angular Components end
   ],
   providers: [],
   bootstrap: [AppComponent]
